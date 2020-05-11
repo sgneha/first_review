b3bfb7d3ec393dc6be167dd80fe98cb61c63691f
sgneha <sgneha05@gmail.com>
2020-05-11 12:31:40 +0100

    when two greens are then it counts

--- lib/report.rb
+++ lib/report.rb
@@ -1,3 +1,14 @@
-def report(_grades)
-  'Green: 1'
- end
+def report(grades)
+  g_count = 0
+  grades_arr = grades.split(',')
+  grades_arr.each do |grade|
+    if grade == 'Green'
+      g_count += 1
+    elsif grades == 'Red'
+      'Red: 1'
+    else
+      'Amber: 1'
+     end
+  end
+  "Green: #{g_count}"
+end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -4,4 +4,13 @@ describe 'generate report' do
   it 'when one green' do
     expect(report('Green')). to eq 'Green: 1'
   end
+  it 'when one red' do
+    expect(report('Red')). to eq 'Red: 1'
+  end
+  it 'when one amber' do
+    expect(report('Amber')). to eq 'Amber: 1'
+  end
+  it 'when two greens' do
+    expect(report('Green,Green')). to eq 'Green: 2'
+  end
 end
